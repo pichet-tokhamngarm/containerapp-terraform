20388772cac2c61a81ebeabece86dcfc3382be6a
Pichet Tokhamngarm <74438293+pichet-t@users.noreply.github.com>
2023-05-07 16:33:33 +0700

    Changes to be committed: 	modified:   terraform/containerapp/main.tf

--- terraform/containerapp/main.tf
+++ terraform/containerapp/main.tf
@@ -8,7 +8,7 @@ data "azurerm_log_analytics_workspace" "la" {
 }
 
 data "azurerm_container_registry" "acr" {
-  name                = "${var.app_name}acr"
+  name                = replace("${var.name}acr", "-", "")
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
